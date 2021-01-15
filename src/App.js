af8952bcd8147c686d8467975b5fd3c1c80f0a85
borhen10 <73178497+borhen10@users.noreply.github.com>
2021-01-16 00:34:23 +0100

    movie-app-checkpoint

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+import {Data} from './data';
+import {MovieList} from './component/MovieList';
+import {FilterV2} from './component/FilterV2';
+import {useState} from 'react';
 
 function App() {
+  const [anime, setAnime] = useState(Data);
+  const [rateValue, setRateValue] = useState(5);
+  const [inputFilter, setInputFilter] = useState('');
+  const filterHandler = (searchTerm) => {
+    setInputFilter(searchTerm);
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <FilterV2 ratevalue={rateValue} setratevalue={setRateValue} searching={filterHandler}/>
+      <MovieList animelist={anime.filter(el => el.rate <= rateValue && el.title.toLocaleLowerCase().includes(inputFilter.toLocaleLowerCase()))} setanime={setAnime} />
     </div>
   );
 }
---
+++ src/data.js
@@ -0,0 +1,85 @@
+export const Data = [
+    {
+    title: "Naruto Shippuden",
+    posterUrl: "https://static.displate.com/857x1200/displate/2020-04-14/f816f1c64d1a46b67b34c20546673859_ba3b501f803cb4f72f7c20b0db422927.jpg",
+    rate: 5,
+    description: 'Naruto Uzumaki, is a loud, hyperactive, adolescent ninja who constantly searches for approval and recognition, as well as to become Hokage, who is acknowledged as the leader and strongest of all ninja in the village.'},
+    {
+    title: "Black Clover",
+    posterUrl: "https://static.displate.com/857x1200/displate/2020-02-18/06370f834c61107777c20b5bd0e7e5a2_f2b71d649b76a53e64211db5fe334d60.jpg",
+    rate: 4,
+    description: 'Asta and Yuno were abandoned together at the same church and have been inseparable since. As children, they promised that they would compete against each other to see who would become the next Emperor Magus. However, latrine as they grew up, some differences between them became plain. Yuno was a genius with magic, with amazing power and control, while Asta could not use magic at all, and tried to make up for his lack by training physically. When they received their Grimoires at age 15, Yuno got a spectacular book with a four-leaf clover (most people receive a three-leaf-clover), while Asta received nothing at all. However, when Yuno was threatened, the truth about Asta power was revealed, he received a five-leaf clover Grimoire, a black clover. Now the two friends are heading out in the world, both seeking the same goal.'},
+    {
+    title: "Hunter x Hunter",
+    posterUrl: "https://static.displate.com/857x1200/displate/2019-11-12/8b4744175be6627262352847416162d8_208c7192d992b246aebd9d599f47a43d.jpg",
+    rate: 5,
+    description: 'The story begins with a young boy named Gon Freecss, who one day discovers that the father whom he thought was dead, is in fact alive and well. He learns that his father, Ging, is a legendary "Hunter", an individual who has proven themselves an elite member of humanity. Despite the fact that Ging left his son with his relatives in order to pursue his own dreams, Gon becomes determined to follow in his father footsteps, pass the rigorous Hunter Examination, and eventually find his father to become a Hunter in his own right and wishing to see his father one day.'},
+    {
+    title: "One Punch Man",
+    posterUrl: "https://static.displate.com/857x1200/displate/2020-05-11/0ec20fd769fa405e67c2f65c57a4dc00_bf93a9b62e6bdaed153337a9cb692358.jpg",
+    rate: 3,
+    description: 'In a world of superhuman beings, Saitama is a unique hero, he can defeat enemies with a single punch. But being just one hero in a world filled with them, his life is empty and hollow: he gets no respect from anyone, he displays a laidback attitude to everything and for the most part, he finds his overall hero life pointless... and worst of all, he lost his hair due to intense training. These are the adventures of an ordinary yet extraordinary hero.'},
+    {
+    title: "My Hero Academia",
+    posterUrl: "https://static.displate.com/857x1200/displate/2020-08-11/e485a785d31bd35e2d00a55076bf72ef_5b52517c39bd2ec54f68477a84407cc4.jpg",
+    rate: 4,
+    description: 'In a world populated with superhumans, the superhero-loving Izuku Midoriya is without power. However, after the Quirkless dreamer Izuku inherits the powers of the world best superhero, All Might, his hopes of becoming the top hero are now possible. Once enrolled in the high school for heroes, U.A., Izuku soon discovers being a hero is much more complicated than it appears.'},
+    {
+    title: "Vinland Saga",
+    posterUrl: "https://static.displate.com/857x1200/displate/2020-01-26/afd8ac44a8757c96b6cfeac301b605f0_5a9b5e7410accaac7792526e385cbbd7.jpg",
+    rate: 3,
+    description: 'A young man named Thorfinn finds himself in a quest for revenge against his father killer. Firstly indulged in the aparent greatness of war and honor, Thorfinn quickly changes as he endures having to survive alone and then alongside the man he vows to kill, developing conflicting emotions towards the causality of his past and present life.'},
+    {
+    title: "Attack on Titan",
+    posterUrl: "https://static.displate.com/857x1200/displate/2020-06-01/7d95ed3621d9d0117e590cc73a6c5373_8b821c8b91a1985c8e9617071a3c714f.jpg",
+    rate: 4,
+    description: 'Set in a fantasy world, mankind is driven to the brink of extinction by mindless, man-eating giants known as Titans. In defense, all of humanity retreated to a civilisation contained within three concentric 50-meter Walls: Maria, Rose, and Sina. In the year 845, after nearly a century of relative peace within the walls, a 60-meter "Colossal" Titan materialised at and destroyed the outermost gate, ushering in hoards of smaller Titans. Witnessing the fullness of the Titan horror first-hand, a young and tenacious Eren Jaeger vows to rid the world of all Titans and win back freedom for mankind. He is joined by his adoptive sister, Mikasa Ackerman and their friend, Armin Arlert as they set out to fulfill this shared dream.'},
+    {
+    title: "Death Note",
+    posterUrl: "https://static.displate.com/857x1200/displate/2020-06-24/3cef655e44ed3cf0fc0cfcc3d5ee9ab6_906130f6425c9f7818847c7717abbb0d.jpg",
+    rate: 5,
+    description: "An intelligent high schooler who wants justice in the world and wants to be God of the new world he has created by destroying all evil. He plans on achieving this by using a strange supernatural book that fell from another realm onto earth. He tries to cleanse the world from all darkness but he runs into obstacles with the police and a strange mysterious detective known as 'L'"
+    },
+    {
+    title: "Tokyo Ghoul",
+    posterUrl: "https://static.displate.com/857x1200/displate/2019-12-08/34136deea5a0bdb6f0ce68a3a6eabfe0_ba9d6e48731af48a0e20eb6644b2aea6.jpg",
+    rate: 3,
+    description: "In a present-day society where humans live in fear of ravenous hybrid creatures known as ghouls, and a constant internal war is being fought by the Commission of Counter Ghoul (CCG) to rid Tokyo of its many injustices, eighteen-year-old Ken Kaneki finds himself thrust into a world of black and white, human and ghoul, where he must slowly learn that the truth is not as picture-perfect as it seems."
+    },
+    {
+    title: "Fullmetal Alchemist Brotherhood",
+    posterUrl: "https://static.displate.com/857x1200/displate/2020-07-10/8b166f989a8d00145449d80af0418cbb_39f0242458f516f490816fe5b537ccd7.jpg",
+    rate: 5,
+    description: "After their dad left and their mother died, two brothers use alchemy to bring their dead mother back. One loses a arm and a leg, the other one loses his whole body whose soul has been trapped by his brother in an armor. Together, they enlist army and seek for the philosopher's stone to have a chance to fix the biggest mistake of their life."
+    },
+    {
+    title: "One Piece",
+    posterUrl: "https://i.pinimg.com/originals/68/5a/ce/685ace551d172e80512d18377881256c.jpg",
+    rate: 5,
+    description: "There once lived a pirate named Gol D. Roger. He obtained wealth, fame, and power to earn the title of Pirate King. When he was captured and about to be executed, he revealed that his treasure called One Piece was hidden somewhere at the Grand Line. This made all people set out to search and uncover the One Piece treasure, but no one ever found the location of Gol D. Roger's treasure, and the Grand Line was too dangerous a place to overcome. Twenty-two years after Gol D. Roger's death, a boy named Monkey D. Luffy decided to become a pirate and search for Gol D. Roger's treasure to become the next Pirate King."
+    },
+    {
+    title: "The Seven Deadly Sins",
+    posterUrl: "https://static.displate.com/857x1200/displate/2019-12-04/a54cfeb924abc7a7c067a931e71a8c55_dee8599bbf09b52d2be02c5ac1e5f70f.jpg",
+    rate: 2,
+    description: "The Seven Deadly Sins were once a group of knights in the region of Britannia, who disbanded after they supposedly plotted to overthrow the Liones Kingdom. Their supposed defeat came at the hands of the Holy Knights, but rumors continued to persist that they were still alive. Ten years later, the Holy Knights staged a coup d'etat and captured the king, becoming the new, tyrannical rulers of the kingdom. The third princess, Elizabeth, then starts out on a journey to find the Seven Deadly Sins and enlist their help in taking back the kingdom."
+    },
+    {
+    title: "Bleach",
+    posterUrl: "https://static.displate.com/857x1200/displate/2020-07-20/31b213f7cadf4fe08534eb42cd539d85_a20d23dda0502d724a2aa8dcebefd9aa.jpg",
+    rate: 5,
+    description: "High school student Ichigo Kurosaki, who has the ability to see ghosts, gains soul reaper powers from Rukia Kuchiki and sets out to save the world from Hollows."
+    },
+    {
+    title: "Mob Psycho 100",
+    posterUrl: "https://static.displate.com/857x1200/displate/2020-02-06/5fab2167bface8631dcb29c50ce9c407_a7cdb9f75eb5ef98458443b64453159e.jpg",
+    rate: 4,
+    description: "A psychic middle school boy tries to live a normal life and keep his growing powers under control, even though he constantly gets into trouble."
+    },
+    {
+    title: "Dr.Stone",
+    posterUrl: "https://static.displate.com/857x1200/displate/2020-08-25/e14e880d552704adc87f84644445267e_22ce1b98c0f0438b659fe95fd19b3d10.jpg",
+    rate: 4,
+    description: "One fateful day, all of humanity was petrified by a blinding flash of light. After several millennia, high schooler Taiju awakens and finds himself lost in a world of statues. However, he's not alone. His science-loving friend Senku's been up and running for a few months and he's got a grand plan in mind, to kickstart civilization with the power of science."
+    },
+];
